f8cb4811b9eb425e5821a66eebb8354d2307441c
seven <419626398@qq.com>
2022-05-24 22:11:26 +0800

    安装路由依赖

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,10 @@
 <script lang="ts" setup>
+import HelloWorld from '@/components/HelloWorld.vue'
 </script>
 
 <template>
   app
+  <HelloWorld />
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
