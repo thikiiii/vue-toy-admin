ea4d66da4d0f0986649af2e6eeb711931ddb863d
songpeng <419626398@qq.com>
2022-12-20 20:48:45 +0800

    appSettings完成

--- src/hooks/effect/useGlobalEvents.ts
+++ src/hooks/effect/useGlobalEvents.ts
@@ -8,10 +8,10 @@ export const useGlobalEvents = () => {
     const themeStore = useThemeStore()
     const layoutStore = useLayoutStore()
     const { header, sidebar, footer, app, mobile } = layoutStore.$state
-    const { theme, themeMode, followSystem } = themeStore.$state
 
     // 监听窗口关闭
     useEventListener(window, 'unload', () => {
+        const { theme, themeMode, followSystem } = themeStore.$state
         AppStorage.setSettings({ theme, themeMode, followSystem, header, sidebar, footer, app })
     })
 
